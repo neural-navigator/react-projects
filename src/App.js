83a5958924ac53f6c7b326f5892651a4d60d205a
iamsatyapati <iamsatyapati@gmail.com>
2024-05-10 08:06:50 +0530

    fragments implemented

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@
 // import ColorPallete from "./components/ColorPallete";
 // import PropImplement from "./components/PropImplement";
 import ChildToParentProp from "./components/ChildToParentProp";
+import Fragments from "./components/Fragments";
 
 function App() {
-  const childPropHandler = (val) => console.log(`${val} printed in parent!`)
+  // const childPropHandler = (val) => console.log(`${val} printed in parent!`)
   return (
-    <div>
+    <>
       {/* <Counter /> */}
       {/* <SubscribeManage /> */}
       {/* <ColorPallete /> */}
       {/* <PropImplement btn_color="#ff1111" name="Hello"/> */}
-      <ChildToParentProp printChildProp={childPropHandler} />
-    </div>
+      {/* <ChildToParentProp printChildProp={childPropHandler} /> */}
+      <Fragments />
+    </>
   );
 }
 
---
+++ src/components/Fragments.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Fragments() {
+  return (
+    <>
+      <h1>data</h1>
+    </>
+  )
+}
+
+export default Fragments
+
+// <></>: previously this was written as React.Fragments
